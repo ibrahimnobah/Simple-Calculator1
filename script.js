9e015711f50a013e370337b4ac2f0088c9f817cd
ibrahimnobahe <ibrahim.a.nobah@gmail.com>
2020-03-04 13:58:45 +0200

    UiAndEngine

--- script.js
+++ script.js
@@ -1,62 +1,106 @@
-class Calculator {
+class CalculatorUi {
+  constructor() {
+    this.calculatorEng = new CalculatorEngine();
+  }
+  clickButtons(e) {
+    let number = e.target.dataset.number;
+    var val = this.calculatorEng.appendNumber(number);
+    this.showDisplay(val);
+  }
+
+  clickoperation(e) {
+    let operand = e.getAttribute("data-operation");
+    let oper = this.calculatorEng.chooseOperation(operand);
+    this.showDisplay(oper);
+  }
+
+  onClearClick(){
+    let clear = this.calculatorEng.clear();
+    this.showDisplay("");
+  }
+
+
+  onEqualsClick(){
+    let val = this.calculatorEng.compute();
+    this.showDisplay(val);
+  }
+
+  showDisplay(input) {
+    this.DisplayDivnum.innerText = input;
+  }
+  init() {
+
+    this.DisplayDivnum = document.getElementsByClassName("DisplayDiv")[0];
+    const numberButtons = document.getElementsByClassName("number-button");
+
+    for (let i = 0; i < numberButtons.length; i++) {
+      numberButtons[i].addEventListener("click", this.clickButtons);
+    }
+      const operationButtons = document.getElementsByClassName("operation");
+      for (let i = 0; i < operationButtons.length; i++) {
+        const element = operationButtons[i];
+        element.addEventListener("click", this.clickoperation);
+      }
+
+      const equalsButton = document.getElementsByClassName("equals")[0];
+      equalsButton.addEventListener("click", onEqualsClick);
+
+      const allClearButton = document.getElementsByClassName("clear")[0];
+
+      allClearButton.addEventListener("click",  onClearClick);
+    }
   
+ }
+class CalculatorEngine {
   constructor() {
     this.firstNumber = "";
     this.secondNumber = "";
     this.operation = undefined;
-    this.isfirstNum = true;
-    this.DisplayDivnum = null;
+    this.isfirstNum = false;
   }
 
-  appendNumber(e) {
- 
-    let number = e.target.dataset.number;
-
-      
-    if (calculator.isfirstNum) 
-    {
-      calculator.firstNumber += number;
-      calculator.showDisplay(calculator.firstNumber);
-    } 
+  appendNumber(number) {
+    if (this.isfirstNum) {
+      this.firstNumber += number;
+      return this.firstNumber;
+    }
     else {
-      calculator.secondNumber += number;
-      calculator.showDisplay(calculator.secondNumber);
+      this.secondNumber += number;
+      return this.secondNumber;
     }
   }
-
   chooseOperation(operation) {
-var x=this;
-console.log(x);
     this.isfirstNum = false;
     this.operation = operation;
-    this.showDisplay(operation);
+    return operation;
   }
 
   compute() {
-    let result;
+    let result=0; 
+
     const firstNum = parseFloat(this.firstNumber);
     const secondNum = parseFloat(this.secondNumber);
-if(firstNum!==null)
-    switch (this.operation) {
-      case "+":
-        result = firstNum + secondNum;
-       
-        break;
-      case "-":
-        result = firstNum - secondNum;
-       
-        break;
-      case "*":
-        result = firstNum * secondNum;
-        
-        break;
-      case "÷":
-        result = firstNum / secondNum;
-         
-        break;
-      default:
-        return;
-    }    
+    if (firstNum !== null)
+      switch (this.operation) {
+        case "+":
+          result = firstNum + secondNum;
+
+          break;
+        case "-":
+          result = firstNum - secondNum;
+
+          break;
+        case "*":
+          result = firstNum * secondNum;
+
+          break;
+        case "÷":
+          result = firstNum / secondNum;
+
+          break;
+        default:
+          return;
+      }
     return result;
   }
   clear() {
@@ -64,48 +108,10 @@ if(firstNum!==null)
     this.secondNumber = "";
     this.operation = undefined;
     this.isfirstNum = true;
-    this.showDisplay("");
-  }
-
-  showDisplay(input) {
-    this.DisplayDivnum.innerText = input;
-  }
-
-  init() {
-    this.DisplayDivnum = document.getElementsByClassName("DisplayDiv")[0];
-    // console.log(this.DisplayDivnum[0]);
-
-    const numberButtons = document.getElementsByClassName("number-button");
-
-    for (let i = 0; i < numberButtons.length; i++) {
-      const element = numberButtons[i];
-      element.addEventListener("click",this.appendNumber);
-    }
-
-    const operationButtons = document.getElementsByClassName("operation");
-
-    for (let i = 0; i < operationButtons.length; i++) {
-      const element = operationButtons[i];
-      element.addEventListener("click", () => {
-        let operand = element.getAttribute("data-operation");
-        this.chooseOperation(operand);
-      });
-    }
-    const equalsButton = document.getElementsByClassName("equals")[0];
-
-    equalsButton.addEventListener("click", () => {
-      let val= this.compute();
-      this.showDisplay(val);
-    });
-
-    const allClearButton = document.getElementsByClassName("clear")[0];
-
-    allClearButton.addEventListener("click", () => {
-      this.clear();
-    });
+    
   }
 }
+var Ui = new CalculatorUi();
+document.addEventListener("DOMContentLoaded", Ui.init());
 
-var calculator = new Calculator();
-document.addEventListener("DOMContentLoaded",calculator.init());
-calculator.init();
+
